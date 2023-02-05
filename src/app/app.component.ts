bf1f9dd31bd7fa569ebe14da0c2f77b2953cb3c4
Gerald Arenas <xmoddedangelx@gmail.com>
2023-02-05 12:47:30 -0500

    quick save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myApp';
+
+  usersIP:string | undefined;
+  userslatitude: number | undefined;
+  userslongitude: number | undefined;
+  usersCity:string | undefined;
+  usersState:string | undefined;
+  timeOfScan:string | undefined;
+
+  constructor(private http: HttpClient) {
+    this.getUserIP()
+    this.getCurrentLocation()
+    this.getCurrentTime()
+  }
+
+  getUserIP() {
+    this.http.get('https://api.ipify.org', { responseType: 'text' })
+      .subscribe((ip) => {
+        this.usersIP = ip;
+        console.log(ip);
+      });
+  }
+
+  getCurrentLocation(){
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.userslatitude = position.coords.latitude;
+        this.userslongitude = position.coords.longitude;
+        this.getApproximateLocation();
+      });
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  }
+
+  getApproximateLocation(){
+    fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${this.userslatitude}&lon=${this.userslongitude}`)
+    .then(response => response.json())
+    .then(data => {
+      const address = data.address.road + ", " + data.address.town + ", " + data.address.state + ", " + data.address.country;
+      console.log(`Address: ${address}`);
+      this.usersState = data.address.state
+      this.usersCity = data.address.town
+    })
+    .catch(error => console.error(error));
+  }
+
+  getCurrentTime() {
+    let now = new Date();
+    this.timeOfScan = now.toLocaleString();
+    console.log(this.timeOfScan)
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatExpansionModule} from '@angular/material/expansion';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
